912e21b95d67f944f61b47776f6c8e486e289279
Administrator <Administrator@User -2023QBJTJ>
2024-11-18 13:20:19 +0900

    10

--- project_1/project_1/src/main/java/services/Service.java
+++
@@ -1,8 +0,0 @@
-package services;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Service {
-	public ServiceForward execute(HttpServletRequest req, HttpServletResponse res);
-}
